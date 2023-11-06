70dc7fdbc70663d7833a558c3793c3f01f34622b
Nathan Burns <nate10nate@gmail.com>
2023-11-05 22:02:51 -0500

    routing back to the home page with a back button from the 'add exercies' page

--- src/app/add-exercise/add-exercise.module.ts
+++ src/app/add-exercise/add-exercise.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddExerciseComponent } from './views/add-exercise.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AddExerciseComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [AddExerciseComponent],
 })
 export class AddExerciseModule {}
--- src/app/add-exercise/views/add-exercise.component.ts
+++ src/app/add-exercise/views/add-exercise.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'add-exercise',
   templateUrl: './add-exercise.component.html',
+  styleUrls: ['./add-exercise.component.css'],
 })
 export class AddExerciseComponent {}

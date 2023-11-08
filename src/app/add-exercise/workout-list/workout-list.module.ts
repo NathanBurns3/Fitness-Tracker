28c2eb44d06f79dc6fd51765ae2bd785126c89ba
Nathan Burns <nate10nate@gmail.com>
2023-11-07 21:56:38 -0500

    removed unnecessary modules

--- src/app/add-exercise/workout-list/workout-list.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { WorkoutListComponent } from './views/workout-list.component';
-
-@NgModule({
-  declarations: [WorkoutListComponent],
-  imports: [],
-  exports: [WorkoutListComponent],
-})
-export class WorkoutListModule {}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { SummariesModule } from './summaries/summaries.module';
 
 import { AppRoutingModule } from '../app-routing.module';
 import { HomeComponent } from './views/home.component';
+import { DailySummaryComponent } from './summaries/daily/views/daily-summary.component';
+import { MonthlySummaryComponent } from './summaries/monthly/views/monthly-summary.component';
+import { YearlySummaryComponent } from './summaries/yearly/views/yearly-summary.component';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, SummariesModule],
+  declarations: [
+    HomeComponent,
+    DailySummaryComponent,
+    MonthlySummaryComponent,
+    YearlySummaryComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule],
   exports: [HomeComponent],
   providers: [],
   bootstrap: [HomeComponent],

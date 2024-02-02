eca212d3f7af5825339df634a5b4aea523e47c6e
Nathan Burns <nate10nate@gmail.com>
2024-02-02 18:59:06 -0500

    Add AllMealsComponent to AddMealModule

--- src/app/add-meal/add-meal.module.ts
+++ src/app/add-meal/add-meal.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { AddMealComponent } from './views/add-meal.component';
 import { SearchMealsComponent } from './search-meals/views/search-meals.component';
 import { RouterModule } from '@angular/router';
+import { AllMealsComponent } from './search-meals/all-meals/views/all-meals.component';
 
 @NgModule({
-  declarations: [AddMealComponent, SearchMealsComponent],
+  declarations: [AddMealComponent, SearchMealsComponent, AllMealsComponent],
   imports: [CommonModule, RouterModule],
   exports: [AddMealComponent],
 })
---
+++ src/app/add-meal/search-meals/all-meals/views/all-meals.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'all-meals',
+  templateUrl: './all-meals.component.html',
+})
+export class AllMealsComponent {}

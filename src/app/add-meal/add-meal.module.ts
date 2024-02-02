f86f456cdb88e7e382c9585962b24226cb4bc569
Nathan Burns <nate10nate@gmail.com>
2024-02-02 17:39:39 -0500

    wireframe for the 'add meal' page

--- src/app/add-meal/add-meal.module.ts
+++ src/app/add-meal/add-meal.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddMealComponent } from './views/add-meal.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AddMealComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [AddMealComponent],
 })
 export class AddMealModule {}
--- src/app/add-meal/views/add-meal.component.ts
+++ src/app/add-meal/views/add-meal.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'add-meal',
   templateUrl: './add-meal.component.html',
+  styleUrls: ['./add-meal.component.css'],
 })
 export class AddMealComponent {}

0e49cd9af9950919df3631f42d8e9b2f63186bcd
Nathan Burns <nate10nate@gmail.com>
2024-02-02 19:01:55 -0500

    Add CustomMealsComponent to AddMealModule and update search-meals component

--- src/app/add-meal/add-meal.module.ts
+++ src/app/add-meal/add-meal.module.ts
@@ -4,9 +4,15 @@ import { AddMealComponent } from './views/add-meal.component';
 import { SearchMealsComponent } from './search-meals/views/search-meals.component';
 import { RouterModule } from '@angular/router';
 import { AllMealsComponent } from './search-meals/all-meals/views/all-meals.component';
+import { CustomMealsComponent } from './search-meals/custom-meals/views/custom-meals.component';
 
 @NgModule({
-  declarations: [AddMealComponent, SearchMealsComponent, AllMealsComponent],
+  declarations: [
+    AddMealComponent,
+    SearchMealsComponent,
+    AllMealsComponent,
+    CustomMealsComponent,
+  ],
   imports: [CommonModule, RouterModule],
   exports: [AddMealComponent],
 })
---
+++ src/app/add-meal/search-meals/custom-meals/views/custom-meals.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'custom-meals',
+  templateUrl: './custom-meals.component.html',
+})
+export class CustomMealsComponent {}

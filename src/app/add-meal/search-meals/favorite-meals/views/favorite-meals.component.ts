6b3ba4258f0dd6485a4e06e6c1a0acfe9155f663
Nathan Burns <nate10nate@gmail.com>
2024-02-11 19:24:37 -0500

    Update favorite meals component with new styling and data retrieval

--- src/app/add-meal/search-meals/favorite-meals/views/favorite-meals.component.ts
+++ src/app/add-meal/search-meals/favorite-meals/views/favorite-meals.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IFood } from 'src/app/add-meal/models/food';
+import { FavoriteMealService } from '../services/favorite-meal.service';
 
 @Component({
   selector: 'favorite-meals',
   templateUrl: './favorite-meals.component.html',
 })
-export class FavoriteMealsComponent {}
+export class FavoriteMealsComponent implements OnInit {
+  favoriteMeals: IFood[] = [];
+  starImage = 'assets/star-yellow.png';
+
+  constructor(private favoriteMealService: FavoriteMealService) {}
+
+  ngOnInit(): void {
+    this.favoriteMeals = this.favoriteMealService.getFavoriteMeals();
+  }
+}

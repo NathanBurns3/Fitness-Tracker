65241e89a04505ded3e2a3b656f569fcb3ff7d59
Nathan Burns <nate10nate@gmail.com>
2024-03-09 13:29:41 -0500

    Add account management component and functionality

---
+++ frontend/src/app/user-settings/account-management/account-management.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'account-management',
+  templateUrl: './account-management.component.html',
+})
+export class AccountManagementComponent {
+  changePassword(): void {
+    // Change password logic
+  }
+
+  deleteAccount(): void {
+    // Delete account logic
+  }
+}
--- frontend/src/app/user-settings/user-settings.module.ts
+++ frontend/src/app/user-settings/user-settings.module.ts
@@ -5,12 +5,14 @@ import { RouterModule } from '@angular/router';
 import { PersonalInformationComponent } from './personal-information/personal-information.component';
 import { FormsModule } from '@angular/forms';
 import { ContactInformationComponent } from './contact-information/contact-information.component';
+import { AccountManagementComponent } from './account-management/account-management.component';
 
 @NgModule({
   declarations: [
     UserSettingsComponent,
     PersonalInformationComponent,
     ContactInformationComponent,
+    AccountManagementComponent,
   ],
   imports: [CommonModule, RouterModule, FormsModule],
   exports: [UserSettingsComponent],

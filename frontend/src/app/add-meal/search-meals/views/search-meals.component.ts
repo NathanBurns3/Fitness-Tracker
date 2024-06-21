a3cd3c3c56a3a6befc2cd6d2c48915806dd470e9
Nathan Burns <nate10nate@gmail.com>
2024-06-20 20:16:14 -0400

    Update search-meals component styles for responsive layout

--- frontend/src/app/add-meal/search-meals/views/search-meals.component.ts
+++ frontend/src/app/add-meal/search-meals/views/search-meals.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'search-meals',
   templateUrl: './search-meals.component.html',
+  styleUrls: ['./search-meals.component.css'],
 })
 export class SearchMealsComponent {
   selectedSearch: string = 'All';

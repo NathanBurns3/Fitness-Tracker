578d51374dc32d3df3176f2f6410b654aea1dae6
Nathan Burns <nate10nate@gmail.com>
2024-06-25 17:55:04 -0400

    Refactor account management component for improved responsiveness and styling

--- frontend/src/app/user-settings/account-management/account-management.component.ts
+++ frontend/src/app/user-settings/account-management/account-management.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'account-management',
   templateUrl: './account-management.component.html',
+  styleUrls: ['./account-management.component.css'],
 })
 export class AccountManagementComponent {
   changePassword(): void {

5b46b5dbd754e66d6e3dcc7e688642124c82a6db
Nathan Burns <nate10nate@gmail.com>
2024-07-23 17:29:23 -0400

    Add home route to backend API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,12 +5,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import connectDB from './config/db';
 import { port } from './config/env';
+import homeRouter from './api/routes/homeRoutes';
 
 const app = express();
 
 app.use(bodyParser.json());
 
 connectDB();
+
+app.use('/home', homeRouter);
 
 app.get('/', (req, res) => {
   console.log('Root endpoint hit');

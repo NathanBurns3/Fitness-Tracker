77299f637d5ad43399e0cf75379d787cdfbed5e6
Nathan Burns <nate10nate@gmail.com>
2024-08-01 22:01:26 -0400

    Update DailyExercisesService to use HTTP requests for CRUD operations

--- frontend/src/app/add-exercise/services/daily-exercises.service.ts
+++ frontend/src/app/add-exercise/services/daily-exercises.service.ts
@@ -1,126 +1,119 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { IExercise } from '../models/exercise';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { environment } from 'src/environments/environment.prod';
+import { catchError, map, Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { getHeaders } from 'src/utils/http-headers.util';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DailyExercisesService {
+  private apiUrl = environment.apiURL + '/exercise';
   exerciseAdded = new EventEmitter<void>();
 
-  exercises: IExercise[] = [
-    {
-      exerciseID: '1',
-      muscleGroup: 'Chest',
-      exerciseName: 'Bench Press',
-      sets: 0,
-    },
-    {
-      exerciseID: '2',
-      muscleGroup: 'Chest',
-      exerciseName: 'Incline Bench Press',
-      sets: 0,
-    },
-    {
-      exerciseID: '3',
-      muscleGroup: 'Chest',
-      exerciseName: 'Decline Bench Press',
-      sets: 0,
-    },
-    {
-      exerciseID: '4',
-      muscleGroup: 'Chest',
-      exerciseName: 'Chest Fly',
-      sets: 0,
-    },
-    {
-      exerciseID: '5',
-      muscleGroup: 'Chest',
-      exerciseName: 'Cable Crossover',
-      sets: 0,
-    },
-    {
-      exerciseID: '6',
-      muscleGroup: 'Back',
-      exerciseName: 'Deadlift',
-      sets: 0,
-    },
-    {
-      exerciseID: '7',
-      muscleGroup: 'Back',
-      exerciseName: 'Bent Over Row',
-      sets: 0,
-    },
-    {
-      exerciseID: '8',
-      muscleGroup: 'Back',
-      exerciseName: 'Pull Up',
-      sets: 0,
-    },
-    {
-      exerciseID: '9',
-      muscleGroup: 'Back',
-      exerciseName: 'Lat Pulldown',
-      sets: 0,
-    },
-    {
-      exerciseID: '10',
-      muscleGroup: 'Back',
-      exerciseName: 'Seated Row',
-      sets: 0,
-    },
-    {
-      exerciseID: '11',
-      muscleGroup: 'Back',
-      exerciseName: 'T-Bar Row',
-      sets: 0,
-    },
-    {
-      exerciseID: '12',
-      muscleGroup: 'Back',
-      exerciseName: 'Straight Arm Pulldown',
-      sets: 0,
-    },
-  ];
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) {}
 
-  constructor(private snackBar: MatSnackBar) {}
-
-  getExercises(): IExercise[] {
-    return this.exercises;
+  getExercises(): Observable<IExercise[]> {
+    return this.http.get<IExercise[]>(this.apiUrl + '/dailyExercises', {
+      headers: getHeaders(),
+    });
   }
 
-  addExercise(exercise: IExercise): boolean {
-    if (this.getExercises().length >= 30) {
-      this.snackBar.open('Maxed out the amount of exercises for the day!', '', {
-        duration: 2000,
-      });
-      return false;
-    }
-    this.exercises.push(exercise);
-    this.exerciseAdded.emit();
-    this.snackBar.open(exercise.exerciseName + ' was added!', '', {
-      duration: 2000,
-    });
-    return true;
+  addExercise(exercise: IExercise): Observable<boolean> {
+    return this.http
+      .post<{ success: Boolean; message: string }>(
+        this.apiUrl + '/addDailyExercise',
+        { exercise },
+        {
+          headers: getHeaders(),
+        }
+      )
+      .pipe(
+        map((response) => {
+          if (response.success) {
+            this.exerciseAdded.emit();
+            this.snackBar.open(exercise.exerciseName + ' was added!', '', {
+              duration: 2000,
+            });
+            return true;
+          } else {
+            this.snackBar.open(response.message, '', {
+              duration: 2000,
+            });
+            return false;
+          }
+        }),
+        catchError((error: Error | any) => {
+          this.snackBar.open(error.message, '', {
+            duration: 2000,
+          });
+          return of(false);
+        })
+      );
   }
 
   updateExercise(exercise: IExercise): void {
-    const index = this.exercises.findIndex(
-      (w) => w.exerciseID === exercise.exerciseID
-    );
-    this.exercises[index] = exercise;
-    this.snackBar.open(exercise.exerciseName + ' was edited!', '', {
-      duration: 2000,
-    });
+    this.http
+      .put<{ success: Boolean; message: string }>(
+        this.apiUrl + '/updateDailyExercise',
+        { exercise },
+        {
+          headers: getHeaders(),
+        }
+      )
+      .pipe(
+        map((response) => {
+          if (response.success) {
+            this.snackBar.open(exercise.exerciseName + ' was updated!', '', {
+              duration: 2000,
+            });
+            return true;
+          } else {
+            this.snackBar.open(response.message, '', {
+              duration: 2000,
+            });
+            return false;
+          }
+        }),
+        catchError((error: Error | any) => {
+          this.snackBar.open(error.message, '', {
+            duration: 2000,
+          });
+          return of(false);
+        })
+      );
   }
 
   deleteExercise(exercise: IExercise): void {
-    const index = this.exercises.findIndex(
-      (w) => w.exerciseID === exercise.exerciseID
-    );
-    this.exercises.splice(index, 1);
-    this.snackBar.open(exercise.exerciseName + ' was deleted!', '', {
-      duration: 2000,
-    });
+    this.http
+      .delete<{ success: Boolean; message: string }>(
+        `${this.apiUrl}/deleteDailyExercise/${exercise.exerciseID}`,
+        {
+          headers: getHeaders(),
+        }
+      )
+      .pipe(
+        map((response) => {
+          if (response.success) {
+            this.snackBar.open(exercise.exerciseName + ' was deleted!', '', {
+              duration: 2000,
+            });
+            return true;
+          } else {
+            this.snackBar.open(response.message, '', {
+              duration: 2000,
+            });
+            return false;
+          }
+        }),
+        catchError((error: Error | any) => {
+          this.snackBar.open(error.message, '', {
+            duration: 2000,
+          });
+          return of(false);
+        })
+      );
   }
 }

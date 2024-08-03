65b68f6fc7aeee68e86bbf443a6b7342410c513a
Nathan Burns <nate10nate@gmail.com>
2024-08-03 12:50:05 -0400

    Add home routes and controllers

---
+++ backend/src/api/routes/homeRoutes.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import { getProfileInfo } from '../controllers/home/profileController';
+import {
+  getDailyEatingInfo,
+  getDailyExerciseInfo,
+} from '../controllers/home/dailyInfoController';
+import {
+  getMonthlyBreakdownInfo,
+  getMonthlyExerciseInfo,
+} from '../controllers/home/monthlyInfoController';
+import {
+  getYearlyEatingGoals,
+  getYearlyExerciseGoals,
+} from '../controllers/home/yearlyInfoController';
+import { verifyToken } from '../helpers/authMiddleware';
+
+const homeRouter = express.Router();
+
+homeRouter.get('/profile', verifyToken, getProfileInfo);
+homeRouter.get('/dailyEatingInfo', verifyToken, getDailyEatingInfo);
+homeRouter.get('/dailyExerciseInfo', verifyToken, getDailyExerciseInfo);
+homeRouter.get('/monthlyExerciseInfo', verifyToken, getMonthlyExerciseInfo);
+homeRouter.get('/monthlyBreakdownInfo', verifyToken, getMonthlyBreakdownInfo);
+homeRouter.get('/yearlyEatingGoals', verifyToken, getYearlyEatingGoals);
+homeRouter.get('/yearlyExerciseGoals', verifyToken, getYearlyExerciseGoals);
+
+export default homeRouter;

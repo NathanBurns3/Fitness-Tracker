cecff498c8f3a99dc12d9fee75ed6f92987ae465
Nathan Burns <nate10nate@gmail.com>
2024-08-03 12:50:15 -0400

    Add meal routes and controllers

---
+++ backend/src/api/routes/mealRouter.ts
@@ -0,0 +1,45 @@
+import express from 'express';
+import {
+  addDailyFood,
+  deleteDailyFood,
+  getDailyFoods,
+  updateDailyFood,
+} from '../controllers/meals/dailyFoodController';
+import {
+  addFavoriteMeal,
+  deleteFavoriteMeal,
+  getFavoriteMeals,
+  getFavoriteMealsLimit,
+} from '../controllers/meals/favoriteMealsController';
+import {
+  addCustomMeal,
+  deleteCustomMeal,
+  getCustomMeal,
+  getCustomMeals,
+  updateCustomMeal,
+} from '../controllers/meals/customMealsController';
+import {
+  searchMeals,
+  updateNutritions,
+} from '../controllers/meals/foodLookupController';
+import { verifyToken } from '../helpers/authMiddleware';
+
+const mealRouter = express.Router();
+
+mealRouter.get('/dailyFoods', verifyToken, getDailyFoods);
+mealRouter.post('/addDailyFood', verifyToken, addDailyFood);
+mealRouter.put('/updateDailyFood', verifyToken, updateDailyFood);
+mealRouter.delete('/deleteDailyFood', verifyToken, deleteDailyFood);
+mealRouter.get('/favoriteMeals', verifyToken, getFavoriteMeals);
+mealRouter.post('/addFavoriteMeal', verifyToken, addFavoriteMeal);
+mealRouter.delete('/deleteFavoriteMeal', verifyToken, deleteFavoriteMeal);
+mealRouter.get('/favoriteMealsLimit', verifyToken, getFavoriteMealsLimit);
+mealRouter.get('/customMeals', verifyToken, getCustomMeals);
+mealRouter.get('/customMeal', verifyToken, getCustomMeal);
+mealRouter.post('/addCustomMeal', verifyToken, addCustomMeal);
+mealRouter.put('/updateCustomMeal', verifyToken, updateCustomMeal);
+mealRouter.delete('/deleteCustomMeal', verifyToken, deleteCustomMeal);
+mealRouter.get('/searchFoods', verifyToken, searchMeals);
+mealRouter.put('/updateNutritions', verifyToken, updateNutritions);
+
+export default mealRouter;

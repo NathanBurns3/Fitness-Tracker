6dab1d2d6e1812fd75f41ba8ccda261b0b435449
Nathan Burns <nate10nate@gmail.com>
2024-08-04 11:54:01 -0400

    Update custom meal ID type to use mongoose.Types.ObjectId

--- backend/src/api/models/api/meals/custom-meal.ts
+++ backend/src/api/models/api/meals/custom-meal.ts
@@ -1,7 +1,8 @@
+import mongoose from 'mongoose';
 import { IFood } from '../meals/food';
 
 export interface ICustomMeal {
-  id: number;
+  id: mongoose.Types.ObjectId;
   name: string;
   servingUnit: string;
   servingSize: number;

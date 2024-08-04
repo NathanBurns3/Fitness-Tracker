ee43105e62ad427cbb3680eea4e0936116002ec4
Nathan Burns <nate10nate@gmail.com>
2024-08-04 17:31:37 -0400

    Refactor mealRouter to use dynamic mealID in getCustomMeal endpoint

--- frontend/src/app/add-meal/search-meals/custom-meals/services/custom-meals.service.ts
+++ frontend/src/app/add-meal/search-meals/custom-meals/services/custom-meals.service.ts
@@ -1,335 +1,124 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { catchError, map, Observable, of } from 'rxjs';
 import { ICustomMeal } from 'src/app/add-meal/models/custom-meal';
 import { IFood } from 'src/app/add-meal/models/food';
+import { environment } from 'src/environments/environment';
+import { getHeaders } from 'src/utils/http-headers.util';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CustomMealService {
-  customMeals: ICustomMeal[] = [
-    {
-      id: '1',
-      name: 'Custom Meal 1',
-      servingUnit: 'g',
-      servingSize: 1,
-      food: [
-        {
-          fdcID: 1,
-          description: 'Apple',
-          brandName: 'Generic',
-          servingSize: 1,
-          servingUnit: 'apple',
-          packageWeight: '18g',
-          ingredients: 'apple',
-          nutritions: {
-            calories: 95,
-            protein: 0.5,
-            carbs: 25,
-            fat: 0.3,
-            fiber: 4.4,
-          },
-        },
-        {
-          fdcID: 2,
-          description: 'Banana',
-          brandName: 'Generic',
-          servingSize: 1,
-          servingUnit: 'banana',
-          packageWeight: '18g',
-          ingredients: 'banana',
-          nutritions: {
-            calories: 105,
-            protein: 1.3,
-            carbs: 27,
-            fat: 0.4,
-            fiber: 3.1,
-          },
-        },
-      ],
-    },
-    {
-      id: '2',
-      name: 'Custom Meal 2',
-      servingUnit: 'g',
-      servingSize: 50,
-      food: [
-        {
-          fdcID: 3,
-          description: 'Orange',
-          brandName: 'Generic',
-          servingSize: 1,
-          servingUnit: 'orange',
-          packageWeight: '18g',
-          ingredients: 'orange',
-          nutritions: {
-            calories: 62,
-            protein: 1.2,
-            carbs: 15,
-            fat: 0.2,
-            fiber: 3.1,
-          },
-        },
-        {
-          fdcID: 4,
-          description: 'Grapes',
-          brandName: 'Generic',
-          servingSize: 1,
-          servingUnit: 'grape',
-          packageWeight: '18g',
-          ingredients: 'grape',
-          nutritions: {
-            calories: 62,
-            protein: 1.2,
-            carbs: 15,
-            fat: 0.2,
-            fiber: 3.1,
-          },
-        },
-      ],
-    },
-    {
-      id: '3',
-      name: 'Custom Meal 3',
-      servingUnit: 'g',
-      servingSize: 10,
-      food: [
-        {
-          fdcID: 5,
-          description: 'Strawberry',
-          brandName: 'Generic',
-          servingSize: 1,
-          servingUnit: 'strawberry',
-          packageWeight: '18g',
-          ingredients: 'strawberry',
-          nutritions: {
-            calories: 32,
-            protein: 0.7,
-            carbs: 8,
-            fat: 0.3,
-            fiber: 2,
-          },
-        },
-        {
-          fdcID: 6,
-          description: 'Blueberry',
-          brandName: 'Generic',
-          servingSize: 1,
-          servingUnit: 'blueberry',
-          packageWeight: '18g',
-          ingredients: 'blueberry',
-          nutritions: {
-            calories: 32,
-            protein: 0.7,
-            carbs: 8,
-            fat: 0.3,
-            fiber: 2,
-          },
-        },
-      ],
-    },
-    {
-      id: '4',
-      name: 'Custom Meal 4',
-      servingUnit: 'g',
-      servingSize: 100,
-      food: [
-        {
-          fdcID: 7,
-          description: 'Pineapple',
-          brandName: 'Generic',
-          servingSize: 1,
-          servingUnit: 'pineapple',
-          packageWeight: '18g',
-          ingredients: 'pineapple',
-          nutritions: {
-            calories: 50,
-            protein: 0.5,
-            carbs: 13,
-            fat: 0.1,
-            fiber: 2.3,
-          },
-        },
-        {
-          fdcID: 8,
-          description: 'Mango',
-          brandName: 'Generic',
-          servingSize: 1,
-          servingUnit: 'mango',
-          packageWeight: '18g',
-          ingredients: 'mango',
-          nutritions: {
-            calories: 60,
-            protein: 0.8,
-            carbs: 15,
-            fat: 0.4,
-            fiber: 1.6,
-          },
-        },
-      ],
-    },
-    {
-      id: '5',
-      name: 'Custom Meal 5',
-      servingUnit: 'g',
-      servingSize: 1,
-      food: [
-        {
-          fdcID: 9,
-          description: 'Peach',
-          brandName: 'Generic',
-          servingSize: 1,
-          servingUnit: 'peach',
-          packageWeight: '18g',
-          ingredients: 'peach',
-          nutritions: {
-            calories: 59,
-            protein: 1.4,
-            carbs: 15,
-            fat: 0.4,
-            fiber: 2.3,
-          },
-        },
-        {
-          fdcID: 10,
-          description: 'Pear',
-          brandName: 'Generic',
-          servingSize: 1,
-          servingUnit: 'pear',
-          packageWeight: '18g',
-          ingredients: 'pear',
-          nutritions: {
-            calories: 57,
-            protein: 0.6,
-            carbs: 15,
-            fat: 0.2,
-            fiber: 3.1,
-          },
-        },
-      ],
-    },
-    {
-      id: '6',
-      name: 'Custom Meal 6',
-      servingUnit: 'g',
-      servingSize: 1,
-      food: [
-        {
-          fdcID: 11,
-          description: 'Kiwi',
-          brandName: 'Generic',
-          servingSize: 1,
-          servingUnit: 'kiwi',
-          packageWeight: '18g',
-          ingredients: 'kiwi',
-          nutritions: {
-            calories: 61,
-            protein: 1.1,
-            carbs: 15,
-            fat: 0.5,
-            fiber: 3,
-          },
-        },
-        {
-          fdcID: 12,
-          description: 'Watermelon',
-          brandName: 'Generic',
-          servingSize: 1,
-          servingUnit: 'watermelon',
-          packageWeight: '18g',
-          ingredients: 'watermelon',
-          nutritions: {
-            calories: 30,
-            protein: 0.6,
-            carbs: 8,
-            fat: 0.2,
-            fiber: 0.4,
-          },
-        },
-      ],
-    },
-    {
-      id: '7',
-      name: 'Custom Meal 7',
-      servingUnit: 'g',
-      servingSize: 1,
-      food: [
-        {
-          fdcID: 13,
-          description: 'Cantaloupe',
-          brandName: 'Generic',
-          servingSize: 1,
-          servingUnit: 'cantaloupe',
-          packageWeight: '18g',
-          ingredients: 'cantaloupe',
-          nutritions: {
-            calories: 34,
-            protein: 0.8,
-            carbs: 8,
-            fat: 0.2,
-            fiber: 1.3,
-          },
-        },
-        {
-          fdcID: 14,
-          description: 'Honeydew',
-          brandName: 'Generic',
-          servingSize: 1,
-          servingUnit: 'honeydew',
-          packageWeight: '18g',
-          ingredients: 'honeydew',
-          nutritions: {
-            calories: 36,
-            protein: 0.5,
-            carbs: 9,
-            fat: 0.2,
-            fiber: 0.9,
-          },
-        },
-      ],
-    },
-  ];
+  private apiUrl = environment.apiURL + '/meal';
 
-  constructor(private snackBar: MatSnackBar) {}
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) {}
 
-  getCustomMeals(): ICustomMeal[] {
-    return this.customMeals;
-  }
-
-  getCustomMeal(id: string): ICustomMeal | undefined {
-    return this.customMeals.find((m) => m.id === id);
-  }
-
-  deleteCustomMeal(id: string): void {
-    const meal: ICustomMeal | undefined = this.customMeals.find(
-      (m) => m.id === id
-    );
-    const index: number = this.customMeals.findIndex((m) => m.id === id);
-
-    if (meal) {
-      this.snackBar.open(meal.name + ' was removed!', '', {
-        duration: 2000,
-      });
-      this.customMeals.splice(index, 1);
-    }
-  }
-
-  updateCustomMeal(meal: ICustomMeal, action: string): void {
-    if (action === 'add') {
-      this.customMeals.push(meal);
-      this.snackBar.open(meal.name + ' was added!', '', {
-        duration: 2000,
-      });
-      return;
-    }
-    const index: number = this.customMeals.findIndex((m) => m.id === meal.id);
-    this.customMeals[index] = meal;
-    this.snackBar.open(meal.name + ' was updated!', '', {
-      duration: 2000,
+  getCustomMeals(): Observable<ICustomMeal[]> {
+    return this.http.get<ICustomMeal[]>(this.apiUrl + '/customMeals', {
+      headers: getHeaders(),
     });
   }
 
-  getNumberOfCustomMeals(): number {
-    return this.customMeals.length;
+  getCustomMeal(id: string): Observable<ICustomMeal> {
+    return this.http.get<ICustomMeal>(this.apiUrl + '/customMeal/' + id, {
+      headers: getHeaders(),
+    });
+  }
+
+  deleteCustomMeal(id: string): Observable<boolean> {
+    return this.http
+      .delete<{ success: boolean; message: string }>(
+        this.apiUrl + '/deleteCustomMeal/' + id,
+        {
+          headers: getHeaders(),
+        }
+      )
+      .pipe(
+        map((response) => {
+          if (response.success) {
+            this.snackBar.open('Meal was removed!', '', {
+              duration: 2000,
+            });
+            return true;
+          } else {
+            this.snackBar.open(response.message, '', {
+              duration: 2000,
+            });
+            return false;
+          }
+        }),
+        catchError((error: Error | any) => {
+          this.snackBar.open(error.message, '', {
+            duration: 2000,
+          });
+          return of(false);
+        })
+      );
+  }
+
+  updateCustomMeal(meal: ICustomMeal, action: string): Observable<boolean> {
+    if (action === 'add') {
+      return this.http
+        .post<{ success: boolean; message: string }>(
+          this.apiUrl + '/addCustomMeal',
+          { customMeal: meal },
+          {
+            headers: getHeaders(),
+          }
+        )
+        .pipe(
+          map((response) => {
+            if (response.success) {
+              this.snackBar.open(meal.name + ' was added!', '', {
+                duration: 2000,
+              });
+              return true;
+            } else {
+              this.snackBar.open(response.message, '', {
+                duration: 2000,
+              });
+              return false;
+            }
+          }),
+          catchError((error: Error | any) => {
+            this.snackBar.open(error.message, '', {
+              duration: 2000,
+            });
+            return of(false);
+          })
+        );
+    } else {
+      return this.http
+        .put<{ success: boolean; message: string }>(
+          this.apiUrl + '/updateCustomMeal',
+          { customMeal: meal },
+          {
+            headers: getHeaders(),
+          }
+        )
+        .pipe(
+          map((response) => {
+            if (response.success) {
+              this.snackBar.open(meal.name + ' was updated!', '', {
+                duration: 2000,
+              });
+              return true;
+            } else {
+              this.snackBar.open(response.message, '', {
+                duration: 2000,
+              });
+              return false;
+            }
+          }),
+          catchError((error: Error | any) => {
+            this.snackBar.open(error.message, '', {
+              duration: 2000,
+            });
+            return of(false);
+          })
+        );
+    }
   }
 }

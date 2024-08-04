0dbe9215872b2d9455c43707858ec8f2403f275e
Nathan Burns <nate10nate@gmail.com>
2024-08-04 16:37:05 -0400

    Add foodLookupController for searching meals and updating nutritions

---
+++ backend/src/api/controllers/meals/foodLookupController.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from 'express';
+import {
+  formatMealData,
+  formatFoodNutritions,
+} from '../../helpers/formatHelper';
+import axios from 'axios';
+import { IFood } from '../../models/api/meals/food';
+
+const clientID = process.env.CLIENT_ID;
+
+export const searchMeals = async (req: Request, res: Response) => {
+  const meal = req.query.meal as string;
+  const url = `https://api.nal.usda.gov/fdc/v1/foods/search?query=${meal}&pageSize=10&requireAllWords=true`;
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        'x-api-key': clientID,
+      },
+    });
+    const meals = formatMealData(response.data);
+    res.json(meals);
+  } catch (error: Error | any) {
+    res.status(500).send(error.message);
+  }
+};
+
+export const updateNutritions = async (req: Request, res: Response) => {
+  const meal = req.body as IFood;
+  const url = `https://api.nal.usda.gov/fdc/v1/foods?fdcIds=${meal.fdcID}`;
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        'x-api-key': clientID,
+      },
+    });
+    formatFoodNutritions(response.data, meal);
+    res.json(meal);
+  } catch (error: Error | any) {
+    res.status(500).send(error.message);
+  }
+};

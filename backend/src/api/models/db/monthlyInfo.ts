7b8590e104fd492bcb56d68a24046856ee62d689
Nathan Burns <nate10nate@gmail.com>
2024-08-05 16:58:19 -0400

    Refactor MonthlyInfo model to use IMonthlyInfoDB interface

--- backend/src/api/models/db/monthlyInfo.ts
+++ backend/src/api/models/db/monthlyInfo.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
-interface IMonthlyInfo extends Document {
+interface IMonthlyInfoDB extends Document {
   userID: mongoose.Types.ObjectId;
   sets: Array<{
     muscleGroup:
@@ -24,41 +24,48 @@ interface IMonthlyInfo extends Document {
   }>;
 }
 
-const MonthlyInfoSchema: Schema = new Schema({
-  userID: { type: Schema.Types.ObjectId, required: true },
-  sets: {
-    type: [
-      {
-        muscleGroup: {
-          type: String,
-          required: true,
-          enum: [
-            'Abs',
-            'Back',
-            'Biceps',
-            'Calves',
-            'Chest',
-            'Forearms',
-            'Glutes',
-            'Hamstrings',
-            'Quads',
-            'Shoulders',
-            'Triceps',
-          ],
+const MonthlyInfoSchema: Schema = new Schema(
+  {
+    userID: { type: Schema.Types.ObjectId, required: true },
+    sets: {
+      type: [
+        {
+          _id: false,
+          muscleGroup: {
+            type: String,
+            required: true,
+            enum: [
+              'Abs',
+              'Back',
+              'Biceps',
+              'Calves',
+              'Chest',
+              'Forearms',
+              'Glutes',
+              'Hamstrings',
+              'Quads',
+              'Shoulders',
+              'Triceps',
+            ],
+          },
+          sets: { type: Number, required: true },
         },
-        sets: { type: Number, required: true },
-      },
-    ],
+      ],
+    },
+    goalsCompleted: {
+      type: [
+        {
+          _id: false,
+          day: { type: Date, required: true },
+          exerciseGoal: { type: Boolean, required: true },
+          eatingGoal: { type: Boolean, required: true },
+        },
+      ],
+    },
   },
-  goalsCompleted: {
-    type: [
-      {
-        day: { type: Date, required: true },
-        exerciseGoal: { type: Boolean, required: true },
-        eatingGoal: { type: Boolean, required: true },
-      },
-    ],
-  },
-});
+  {
+    collection: 'MonthlyInfo',
+  }
+);
 
-export default mongoose.model<IMonthlyInfo>('MonthlyInfo', MonthlyInfoSchema);
+export default mongoose.model<IMonthlyInfoDB>('MonthlyInfo', MonthlyInfoSchema);

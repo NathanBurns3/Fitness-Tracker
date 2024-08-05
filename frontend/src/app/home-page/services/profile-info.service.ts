60bdc7b4ea9acf692ab8115e0a9c4434c343f9bc
Nathan Burns <nate10nate@gmail.com>
2024-08-05 16:57:53 -0400

    Refactor ProfileInfoService to use HttpClient for API calls

--- frontend/src/app/home-page/services/profile-info.service.ts
+++ frontend/src/app/home-page/services/profile-info.service.ts
@@ -1,23 +1,22 @@
 import { Injectable } from '@angular/core';
 import { IProfileInfo } from '../models/profile-info';
 import { WeightGoalEnum } from 'src/app/user-settings/models/weight-goal-enum';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { getHeaders } from 'src/utils/http-headers.util';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProfileInfoService {
-  getProfileInfo(): IProfileInfo {
-    return {
-      firstName: 'Nathan',
-      lastName: 'Burns',
-      profileImage: './assets/testProfilePicture.jpg',
-      gender: 'Male',
-      age: 20,
-      height: 72,
-      weight: 173,
-      goal: WeightGoalEnum.Maintain,
-      exerciseStreak: 3,
-      eatingGoalStreak: 12,
-    };
+  private apiUrl = environment.apiURL + '/home';
+
+  constructor(private http: HttpClient) {}
+
+  getProfileInfo(): Observable<IProfileInfo> {
+    return this.http.get<IProfileInfo>(this.apiUrl + '/profile', {
+      headers: getHeaders(),
+    });
   }
 }

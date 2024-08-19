6b074be55e9073721f143a8471d0055f930a13d5
Nathan Burns <nate10nate@gmail.com>
2024-08-18 22:34:21 -0400

    installed cron

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,17 +2,27 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
-import bodyParser from 'body-parser';
 import connectDB from './config/db';
 import { port } from './config/env';
 import homeRouter from './api/routes/homeRoutes';
 import exerciseRouter from './api/routes/exerciseRoutes';
 import mealRouter from './api/routes/mealRouter';
 import settingsRouter from './api/routes/settingsRoutes';
+import cors from 'cors';
 
 const app = express();
 
-app.use(bodyParser.json());
+// Nathan: Adjust
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+app.use(
+  cors({
+    origin: 'http://localhost:4200', // Replace with your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  })
+);
 
 connectDB();
 

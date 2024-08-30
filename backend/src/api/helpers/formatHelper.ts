df7fad84d04ebd528db6cf57792a8ed136f17f70
Nathan Burns <nate10nate@gmail.com>
2024-08-30 11:08:43 -0400

    Handle missing values in formatHelper.ts

--- backend/src/api/helpers/formatHelper.ts
+++ backend/src/api/helpers/formatHelper.ts
@@ -12,16 +12,16 @@ export const formatMealData = (data: any): IFood[] => {
       fdcID: food.fdcId,
       description: food.description,
       brandName: food.brandName,
-      servingSize: food.servingSize,
-      servingUnit: food.servingSizeUnit,
+      servingSize: food.servingSize || 1,
+      servingUnit: food.servingSizeUnit || 'item',
       packageWeight: food.packageWeight,
       ingredients: food.ingredients,
       nutritions: {
-        calories: nutrients[Nutrient_Ids.Energy],
-        protein: nutrients[Nutrient_Ids.Protein],
-        carbs: nutrients[Nutrient_Ids.Carbs],
-        fat: nutrients[Nutrient_Ids.Fat],
-        fiber: nutrients[Nutrient_Ids.Fiber],
+        calories: nutrients[Nutrient_Ids.Energy] || 0,
+        protein: nutrients[Nutrient_Ids.Protein] || 0,
+        carbs: nutrients[Nutrient_Ids.Carbs] || 0,
+        fat: nutrients[Nutrient_Ids.Fat] || 0,
+        fiber: nutrients[Nutrient_Ids.Fiber] || 0,
       },
     };
     meals.push(meal);
@@ -31,20 +31,25 @@ export const formatMealData = (data: any): IFood[] => {
 
 export const formatFoodNutritions = (data: any, meal: IFood) => {
   meal.nutritions = {
-    calories: data[0].labelNutrients.calories
-      ? Math.ceil(data[0].labelNutrients.calories.value)
-      : 0,
-    protein: data[0].labelNutrients.protein
-      ? Math.ceil(data[0].labelNutrients.protein.value)
-      : 0,
-    carbs: data[0].labelNutrients.carbohydrates
-      ? Math.ceil(data[0].labelNutrients.carbohydrates.value)
-      : 0,
-    fat: data[0].labelNutrients.fat
-      ? Math.ceil(data[0].labelNutrients.fat.value)
-      : 0,
-    fiber: data[0].labelNutrients.fiber
-      ? Math.ceil(data[0].labelNutrients.fiber.value)
-      : 0,
+    calories:
+      (data[0].labelNutrients.calories &&
+        Math.ceil(data[0].labelNutrients.calories.value)) ||
+      0,
+    protein:
+      (data[0].labelNutrients.protein &&
+        Math.ceil(data[0].labelNutrients.protein.value)) ||
+      0,
+    carbs:
+      (data[0].labelNutrients.carbohydrates &&
+        Math.ceil(data[0].labelNutrients.carbohydrates.value)) ||
+      0,
+    fat:
+      (data[0].labelNutrients.fat &&
+        Math.ceil(data[0].labelNutrients.fat.value)) ||
+      0,
+    fiber:
+      (data[0].labelNutrients.fiber &&
+        Math.ceil(data[0].labelNutrients.fiber.value)) ||
+      0,
   };
 };

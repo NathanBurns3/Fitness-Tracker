1d5d3853ecbad09406006cfb7ee65746dc263f48
Nathan Burns <nate10nate@gmail.com>
2024-09-01 22:17:16 -0400

    Update user settings component to use DietEnum for diet plan

--- backend/src/api/models/api/settings/user-settings.ts
+++ backend/src/api/models/api/settings/user-settings.ts
@@ -2,11 +2,12 @@ import { IActivityGoal } from '../settings/activity-goals';
 import { IContactInformation } from '../settings/contact-information';
 import { IPersonalInformation } from '../settings/personal-information';
 import { IPhysicalMeasurements } from '../settings/physical-measurements';
+import { DietEnum } from './diet-enum';
 
 export interface IUserSettings {
   personalInformation: IPersonalInformation;
   contactInformation: IContactInformation;
   physicalMeasurements: IPhysicalMeasurements;
   activityGoal: IActivityGoal;
-  dietPlan: string;
+  dietPlan: DietEnum;
 }

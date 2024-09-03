60ca615fe97915e660814c1dc3d2481344a53b63
Nathan Burns <nate10nate@gmail.com>
2024-09-03 12:33:12 -0400

    Add login component and template

---
+++ frontend/src/app/auth/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+})
+export class LoginComponent {
+  email = '';
+  password = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
+
+  login() {
+    const encodedEmail = btoa(this.email);
+    const encodedPassword = btoa(this.password);
+
+    this.authService.login(encodedEmail, encodedPassword).subscribe(
+      (response: any) => {
+        localStorage.setItem('token', response.token);
+        this.router.navigate(['/home']);
+      },
+      (error) => {
+        console.error('Login error', error);
+        this.snackBar.open('Incorrect email or password', 'Close', {
+          duration: 3000,
+        });
+      }
+    );
+  }
+}

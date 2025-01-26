91b3247344289dcd58b85b9c329314a653a84931
Nathan Burns <nate10nate@gmail.com>
2025-01-26 08:27:02 -0500

    setup frontend webpack

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiURL: 'https://active-life-tracker.vercel.app', // Nathan: will need to update this to the correct URL (ex: http://192.168.1.100:5001)
+  apiURL: process.env['API_URL'],
 };

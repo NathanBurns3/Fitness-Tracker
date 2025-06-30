22db9ea22cb71d9b479746e87b1fdc459550e525
Nathan Burns <nate10nate@gmail.com>
2025-06-29 22:37:04 -0400

    Refactor typings for NodeJS ProcessEnv to improve structure and clarity

--- frontend/src/utils/typings.util.d.ts
+++ frontend/src/utils/typings.util.d.ts
@@ -1,9 +1,5 @@
-interface ProcessEnv {
-  NG_APP_API_URL: string;
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NG_APP_API_URL: string;
+  }
 }
-
-interface Process {
-  env: ProcessEnv;
-}
-
-declare var process: Process;

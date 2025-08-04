d922c58fdae8be93842f518751744d60ca2c9f77
Nathan Burns <nate10nate@gmail.com>
2025-08-04 17:50:46 -0400

    Add timezone specification to cron job schedules

--- backend/src/api/helpers/scheduler.ts
+++ backend/src/api/helpers/scheduler.ts
@@ -1,6 +1,6 @@
 import cron from 'node-cron';
 import { dailyTask, monthlyTask, yearlyTask } from './tasks';
 
-cron.schedule('0 0 * * *', dailyTask);
-cron.schedule('0 0 1 * *', monthlyTask);
-cron.schedule('0 0 1 1 *', yearlyTask);
+cron.schedule('0 0 * * *', dailyTask, { timezone: 'America/New_York' });
+cron.schedule('0 0 1 * *', monthlyTask, { timezone: 'America/New_York' });
+cron.schedule('0 0 1 1 *', yearlyTask, { timezone: 'America/New_York' });

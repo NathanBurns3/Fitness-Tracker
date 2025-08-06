728078a76da35604158a5565fdcba606f6f4e677
Nathan Burns <nate10nate@gmail.com>
2025-08-05 20:51:02 -0400

    Update cron job schedules to run at 9pm Eastern Time

--- backend/src/api/helpers/scheduler.ts
+++ backend/src/api/helpers/scheduler.ts
@@ -1,6 +1,7 @@
 import cron from 'node-cron';
 import { dailyTask, monthlyTask, yearlyTask } from './tasks';
 
-cron.schedule('0 0 * * *', dailyTask, { timezone: 'America/New_York' });
-cron.schedule('0 0 1 * *', monthlyTask, { timezone: 'America/New_York' });
-cron.schedule('0 0 1 1 *', yearlyTask, { timezone: 'America/New_York' });
+// 9pm Eastern Time = 21:00 America/New_York
+cron.schedule('0 21 * * *', dailyTask, { timezone: 'America/New_York' });
+cron.schedule('0 21 1 * *', monthlyTask, { timezone: 'America/New_York' });
+cron.schedule('0 21 1 1 *', yearlyTask, { timezone: 'America/New_York' });

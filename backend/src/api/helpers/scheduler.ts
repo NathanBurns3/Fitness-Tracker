6527b0f99ce12fae917e3bf4499d80ac6cdebd24
Nathan Burns <nate10nate@gmail.com>
2025-08-05 21:10:10 -0400

    Update cron job schedules to run dailyTask at 9:15pm Eastern Time

--- backend/src/api/helpers/scheduler.ts
+++ backend/src/api/helpers/scheduler.ts
@@ -1,8 +1,8 @@
 import cron from 'node-cron';
 import { dailyTask, monthlyTask, yearlyTask } from './tasks';
 
-// 9pm Eastern Time = 21:00 America/New_York
-cron.schedule('0 21 * * *', dailyTask, { timezone: 'America/New_York' });
-cron.schedule('0 21 1 * *', monthlyTask, { timezone: 'America/New_York' });
-cron.schedule('0 21 1 1 *', yearlyTask, { timezone: 'America/New_York' });
-dailyTask(); // TEMP: Run dailyTask immediately for testing
+// 9:15pm Eastern Time = 21:15 America/New_York
+cron.schedule('15 21 * * *', dailyTask, { timezone: 'America/New_York' });
+cron.schedule('15 21 1 * *', monthlyTask, { timezone: 'America/New_York' });
+cron.schedule('15 21 1 1 *', yearlyTask, { timezone: 'America/New_York' });
+//dailyTask(); // TEMP: Run dailyTask immediately

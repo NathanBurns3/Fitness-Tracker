089c74bfe88152e4ac4d645827008705ba1f5daf
Nathan Burns <nate10nate@gmail.com>
2025-10-09 15:56:48 -0400

    Increase rate limit for general requests from 200 to 300

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,7 +20,7 @@ const app = express();
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 200,
+  max: 300,
   message: { message: 'Too many requests, please try again later.' },
   standardHeaders: true,
   legacyHeaders: false,
